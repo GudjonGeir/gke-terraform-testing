0597bbfb63f4795e779fb92d961118b1be0e7c4f
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2021-09-21 10:06:36 +0000

    --wip-- [skip ci]

---
+++ gke.tf
@@ -0,0 +1,67 @@
+# TODO: Read https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/service_account
+resource "google_service_account" "default" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
+}
+
+resource "google_compute_subnetwork" "custom" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.custom.id
+  secondary_ip_range {
+    range_name    = "services-range"
+    ip_cidr_range = "192.168.1.0/24"
+  }
+
+  secondary_ip_range {
+    range_name    = "pod-ranges"
+    ip_cidr_range = "192.168.64.0/22"
+  }
+}
+
+resource "google_compute_network" "custom" {
+  name                    = "test-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_container_cluster" "primary" {
+  name               = "my-vpc-native-cluster"
+  location           = "us-central1-a"
+
+  network    = google_compute_network.custom.id
+  subnetwork = google_compute_subnetwork.custom.id
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "services-range"
+    services_secondary_range_name = google_compute_subnetwork.custom.secondary_ip_range.1.range_name
+  }
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+# TODO: https://cloud.google.com/kubernetes-engine/docs/how-to/preemptible-vms
+# TODO: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_node_pool
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  cluster    = google_container_cluster.primary.name
+  node_count = 2
+
+  location           = "us-central1-a"
+  node_locations = ["us-central1-a", "us-central1-b"]
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
